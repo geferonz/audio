3f6a5c60dbd8ddf3c224b4f28cca0613bf1be719
achagaev <alexey.chagaev@mera.com>
2023-03-07 04:36:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import ArrowLeft from './arrow-left.svg';
+import ArrowRight from './arrow-right.svg';
+import Player from './Player';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [link, setLink] = useState('');
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  const handleClick = (e) => {
+    setLink('');
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLink(input);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!link.length && (
+        <form onSubmit={handleSubmit}>
+          <label for="input-link">Insert the link</label>
+          <div>
+            <input type="text" name="link" id="input-link" placeholder="https://" defaultValue={input} onChange={handleChange} maxlength="240" autoFocus />
+            <button type="submit"><img src={ArrowRight} alt="img" /></button>
+          </div>
+        </form>
+      )}
+      {Boolean(link.length) && (
+        <div id="player">
+          <button type="button" onClick={handleClick}><img src={ArrowLeft} alt="img" />Back</button>
+          <Player src={link} />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Player.js
@@ -0,0 +1,62 @@
+import './Player.css';
+import { useEffect } from 'react';
+
+function Player({url}) {
+
+  useEffect(() => { 
+    let audio = document.getElementById("audio");
+    let playBtn = document.getElementById("play");
+    let prog = document.getElementById("progress");
+    let playTime = document.getElementById("time-played");
+
+    prog.addEventListener("click", function (e) {
+      audio.currentTime = e.target.value;
+    });
+
+    playBtn.addEventListener("click", function (e) {
+      if (audio.paused) {
+        audio.play();
+        playBtn.classList.remove("fa-play");
+        playBtn.classList.add("fa-pause");
+      } else {
+        audio.pause();
+        playBtn.classList.remove("fa-pause");
+        playBtn.classList.add("fa-play");
+      }
+    });
+
+    function paddZero(i) {
+      if (i < 10) {
+        i = "0" + i;
+      } 
+      return i;
+    }
+
+    audio.ontimeupdate = function() {
+      let played = audio.currentTime;
+      let percent = 100 * (audio.currentTime / audio.duration);
+      prog.value = percent;
+  
+      played = played < 60 ? (
+        "00:" + paddZero(Math.floor(played))
+      ) : (
+        paddZero(Math.floor(played / 60)) + ":" + paddZero(Math.floor(played % 60))
+      );
+
+      playTime.innerHTML = played;
+    } 
+  }, []);
+
+  return (
+    <>
+      <audio id="audio" src={url} />
+      <div id="audio-box">
+        <span id="play" className="fas fa-play"></span>
+        <input id="progress" type="range" defaultValue='0' />
+        <span id="time-played">00:00</span>
+      </div>
+    </>
+  );
+}
+
+export default Player;
